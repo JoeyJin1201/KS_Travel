79945ec72fcf542490205beeaf4ceadad97403ca
Joey <joey831201@gmail.com>
2019-07-01 22:57:45 +0800

    layout fixed

--- js/main.js
+++ js/main.js
@@ -8,10 +8,23 @@ const zone = document.querySelector('#zone-selections');
 const page = document.querySelector('.pageDeck');
 
 let currentPage = 1;
-let selectedZone = '';
+let selectedZone = 'default';
 let changePageClick = false;
 let amount = 0;
 let pageStr = '';
+
+let popularZone = document.querySelectorAll('.popular-list-item');
+
+Array.from(popularZone).forEach(eachZone => {
+  eachZone.addEventListener('click', function (event) {
+    selectedZone = this.dataset.zone;
+    console.log(this.dataset.zone);
+    updateList();
+    // if (!confirm(`sure u want to delete ${this.title}`)) {
+    //   event.preventDefault();
+    // }
+  });
+});
 
 xhr.open('get', 'https://data.kcg.gov.tw/api/action/datastore_search?resource_id=92290ee5-6e61-456f-80c0-249eae2fcc97', true);
 xhr.responseType = 'text';
@@ -40,7 +53,7 @@ function zoneSelectOptions() {
 }
 
 function updateList(e) {
-  if (changePageClick === false) {
+  if (changePageClick === false && selectedZone == 'default') {
     selectedZone = e.target.value;
   }
   amount = parseInt(0);
@@ -48,7 +61,7 @@ function updateList(e) {
   for (let i = 0; i < allData.length; i++) {
     if (allData[i].Zone === selectedZone) {
       amount = amount + 1;
-      if (amount <= currentPage * 8 && amount > (currentPage - 1) * 8) {
+      if (amount <= currentPage * 4 && amount > (currentPage - 1) * 4) {
         listStr += '<li class="content-list-item"><h2 class="item-name">' + allData[i].Name + '</h2>';
         listStr += '<div class="item-img-wrap"><img src=' + allData[i].Picture1 + ' class="item-img"></div>';
         listStr += '<p class="item-zone">' + allData[i].Zone + '</p>';
@@ -65,27 +78,37 @@ function updateList(e) {
     }
   }
   let listTitle = document.querySelector('.list-title');
-  if (selectedZone != 'default') {
-    listTitle.innerHTML = selectedZone;
-  } else {
+  if (amount == 0 || selectedZone == 'default') {
     listTitle.innerHTML = '尚未選擇區域';
+    document.querySelector('.content').classList.add('defaultSize');
+  } else {
+    listTitle.innerHTML = selectedZone;
+    document.querySelector('.content').classList.remove('defaultSize');
   }
   list.innerHTML = listStr;
-  if (amount > 8) {
+  let pageAmount = Math.ceil(amount / 4);
+  // console.log('總頁數:' + pageAmount);
+  if (pageAmount > 1) {
     pageStr = '';
-    pageStr += '<span class="prev">< prev</span>';
-    for (let i = 1; i * 8 <= amount + 7; i++) {
+    pageStr += '<span data-page="p" class="prev">< prev</span>';
+    for (let i = 1; i * 4 <= amount + 3; i++) {
       if (currentPage === i) {
         pageStr += '<span data-page="' + i + '" class="page current">' + i + '</span>';
       } else {
         pageStr += '<span data-page="' + i + '" class="page">' + i + '</span>';
       }
     }
-    pageStr += '<span class="next">next ></span>';
+    pageStr += '<span data-page="n" class="next">next ></span>';
+    page.innerHTML = pageStr;
+    if (currentPage == 1 && selectedZone != 'default') {
+      document.querySelector('.prev').classList.add('unclickable');
+    } else if (currentPage == pageAmount) {
+      document.querySelector('.next').classList.add('unclickable');
+    }
   } else {
     pageStr = '';
+    page.innerHTML = pageStr;
   }
-  page.innerHTML = pageStr;
   changePageClick = false;
 }
 
@@ -94,7 +117,15 @@ function changePage(e) {
     return;
   }
   selectedZone = zone.value;
-  currentPage = parseInt(e.toElement.dataset.page);
+
+  if (isNaN(parseInt(e.toElement.dataset.page)) && e.toElement.dataset.page == 'p') {
+    currentPage -= 1;
+  } else if (isNaN(parseInt(e.toElement.dataset.page)) && e.toElement.dataset.page == 'n') {
+    currentPage += 1;
+  } else {
+    currentPage = parseInt(e.toElement.dataset.page);
+  }
+
   changePageClick = true;
   updateList();
 }
